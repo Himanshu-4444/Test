a8b854eb40d3222c19b398a37093af24af39bf55
Himanshu-4444 <154578231+Himanshu-4444@users.noreply.github.com>
2024-01-16 10:13:59 +0530

    Update fun.py

--- plugins/Extra/fun.py
+++ plugins/Extra/fun.py
@@ -4,7 +4,7 @@
 
 from pyrogram import Client, filters
 
-# AESTHETIC------------ https://telegram.me/Josprojects ------------ #
+# AESTHETIC------------ https://telegram.me/visualmovies1 ------------ #
 
 def aesthetify(string):
     PRINTABLE_ASCII = range(0x21, 0x7f)
